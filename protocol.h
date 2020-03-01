ebe479dc973b89107ffccaf29f267d36022cc2e6
Patrik Arlos <patrik.arlos@gmail.com>
2020-03-01 09:37:32 +0100

    Update protocol.h
    Clarified what datatypes needed conversion when transmitted accross the network

--- protocol.h
+++ protocol.h
@@ -17,28 +17,28 @@
    client->server type = 2. 
  */
 struct calcProtocol{
-  uint16_t type;  // What message is this, 1 = server to client, 2 client to server, 3... reserved 
-  uint16_t major_version; // 1
-  uint16_t minor_version; // 0
-  uint32_t id; // Server side identification with operation. Client must return the same ID as it got from Server.
+  uint16_t type;  // What message is this, 1 = server to client, 2 client to server, 3... reserved , conversion needed (for practice)
+  uint16_t major_version; // 1, conversion needed (for practice)
+  uint16_t minor_version; // 0, conversion needed (for practice)
+  uint32_t id; // Server side identification with operation. Client must return the same ID as it got from Server., conversion needed (for practice)
   uint32_t arith; // What operation to perform, see mapping below. 
-  int32_t inValue1; // integer value 1
-  int32_t inValue2; // integer value 2
-  int32_t inResult; // integer result
-  double flValue1;  // float value 1
-  double flValue2;  // float value 2
-  double flResult;  // float result
+  int32_t inValue1; // integer value 1, conversion needed (for practice)
+  int32_t inValue2; // integer value 2, conversion needed (for practice)
+  int32_t inResult; // integer result, conversion needed (for practice)
+  double flValue1;  // float value 1,NO NEED TO do host to Network or Network to Host conversion here, we are using equivalent platforms        
+  double flValue2;  // float value 2,NO NEED TO do host to Network or Network to Host conversion here, we are using equivalent platforms
+  double flResult;  // float result,NO NEED TO do host to Network or Network to Host conversion here, we are using equivalent platforms
 };
 
   
 struct calcMessage {
-  uint16_t type;    // See below
-  uint32_t message; // See below
+  uint16_t type;    // See below, conversion needed (for practice)
+  uint32_t message; // See below, conversion needed (for practice)
   
   // Protocol, UDP = 17, TCP = 6, other values are reserved. 
-  uint16_t protocol;
-  uint16_t major_version; // 1
-  uint16_t minor_version; // 0 
+  uint16_t protocol; // conversion needed (for practice)
+  uint16_t major_version; // 1, conversion needed (for practice)
+  uint16_t minor_version; // 0 , conversion needed (for practice)
 
 };
 
