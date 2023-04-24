7e98c3aaaca66732682ae92d7d69ec593392c5f7
node189 <node189@icloud.com>
2023-04-24 16:25:34 +0800

    feat: add ipv6 support

--- clientmain.cpp
+++ clientmain.cpp
@@ -1,5 +1,6 @@
 #include <arpa/inet.h>
 #include <cstdio>
+#include <cstdlib>
 #include <cstring>
 #include <iostream>
 #include <netdb.h>
@@ -16,53 +17,52 @@
 
 #define DEBUG
 
-const int gMaxSendTimes = 3;
+const int SEND_TIMES = 3;
 const int BUF_SIZE = 100;
 const int LOCAL_PORT = 12345; // Local listening port
 
-void parse_ip_port(const char *str, char *host, int *port) {
+/**
+ * @brief Parse host:port like string
+ *
+ * @param str host:port like string
+ * @param host host variable
+ * @param port port variable
+ */
+void parse_ip_port(const char *str, char *host, char *port) {
+  int len = strlen(str);
+
   const char *colon = strrchr(str, ':');
   if (colon != NULL) {
-    int len = colon - str;
-    strncpy(host, str, len);
-    *(host + len) = '\0';
-    *port = atoi(colon + 1);
+    int host_len = colon - str;
+    strncpy(host, str, host_len);
+    *(host + host_len) = '\0';
+    strncpy(port, colon + 1, len - host_len - 1);
   } else {
     printf("Error: Invalid ip:port format\n");
   }
 }
 
 /**
- * @brief Translate host to IP
+ * @brief parse the host and the port into addrinfo struct
  *
+ * @param host
+ * @param port
+ * @return
  */
-sockaddr_in *host2addr(const char *host, const int port) {
-  hostent *hostinfo;
-  in_addr **addr_list;
-  sockaddr_in *serv_addr;
+addrinfo *host2addr(const char *host, const char *port) {
+  struct addrinfo hints, *res;
+  int status;
 
-  serv_addr = (sockaddr_in *)malloc(sizeof(sockaddr_in));
-  memset(serv_addr, 0, sizeof(sockaddr_in));
+  memset(&hints, 0, sizeof hints);
+  hints.ai_family = AF_UNSPEC;    // IPv4 or IPv6
+  hints.ai_socktype = SOCK_DGRAM; // UDP
 
-  hostinfo = gethostbyname(host);
-  if (!hostinfo) {
-    printf("Invalid address/ Address not supported\n");
-    return NULL;
+  if ((status = getaddrinfo(host, port, &hints, &res)) != 0) {
+    fprintf(stderr, "getaddrinfo error: %s\n", gai_strerror(status));
+    return nullptr;
   }
 
-  addr_list = (struct in_addr **)hostinfo->h_addr_list;
-
-  // Get IP address
-  char *ip = inet_ntoa(*addr_list[0]);
-
-  if (inet_pton(AF_INET, ip, &(serv_addr->sin_addr)) <= 0) {
-    printf("Invalid address/ Address not supported\n");
-    return NULL;
-  }
-  serv_addr->sin_family = AF_INET;
-  serv_addr->sin_port = htons(port);
-
-  return serv_addr;
+  return res;
 }
 
 /**
@@ -99,52 +99,30 @@ int main(int argc, char *argv[]) {
 
   // Parse host and port
   char host[BUF_SIZE];
-  int port;
-  parse_ip_port(argv[1], host, &port);
+  char port[BUF_SIZE];
+  parse_ip_port(argv[1], host, port);
+  printf("Host %s, and port %s.\n", host, port);
 
-  printf("Host %s, and port %d.\n", host, port);
+  // Get server address
+  addrinfo *serv_addr = host2addr(host, port);
+  if (!serv_addr) {
+    return -1;
+  }
 
   // Create UDP socket
-  int sock = socket(AF_INET, SOCK_DGRAM, 0);
+  int sock = socket(serv_addr->ai_family, serv_addr->ai_socktype, serv_addr->ai_protocol);
   if (sock < 0) {
     printf("Socket creation error\n");
     return -1;
   }
 
-  // Get server address
-  sockaddr_in *serv_addr = host2addr(host, port);
-  if (!serv_addr) {
-    return -1;
-  }
-
-#ifdef DEBUG
-  // Get the local IP and port
-  int trial_sock = socket(AF_INET, SOCK_DGRAM, 0);
-
-  if (connect(trial_sock, (struct sockaddr *)serv_addr, sizeof(*serv_addr)) < 0) {
-    printf("Connection Failed\n");
-    return -1;
-  }
-
-  sockaddr_in addr;
-  socklen_t len = sizeof(addr);
-
-  getsockname(trial_sock, (struct sockaddr *)&addr, &len);
-
-  char *local_host = inet_ntoa(addr.sin_addr);
-  uint16_t local_port = ntohs(addr.sin_port);
-
-  printf("Connected to  %s:%d local %s:%d\n", host, port, local_host, local_port);
-  close(trial_sock);
-#endif
-
   // Bind the local address
   struct sockaddr_in local_addr;
-  local_addr.sin_family = AF_INET;
+  local_addr.sin_family = serv_addr->ai_family;
   local_addr.sin_addr.s_addr = htonl(INADDR_ANY);
   local_addr.sin_port = htons(LOCAL_PORT);
 
-  if (bind(sock, (struct sockaddr *)&local_addr, sizeof(local_addr)) == -1) { // 绑定本地地址和端口号
+  if (bind(sock, (struct sockaddr *)&local_addr, sizeof(local_addr)) == -1) {
     puts("Bind error");
     return -1;
   }
@@ -164,8 +142,8 @@ int main(int argc, char *argv[]) {
   while (true) {
     // send request message
     calcMessage reqMsg = {htons(22), htons(0), htons(17), htons(1), htons(0)};
-    if (sendto(sock, &reqMsg, sizeof(reqMsg), 0, (sockaddr *)serv_addr, sizeof(*serv_addr)) < 0) {
-      printf("Send failed\n");
+    if (sendto(sock, &reqMsg, sizeof(reqMsg), 0, serv_addr->ai_addr, serv_addr->ai_addrlen) < 0) {
+      puts("Send failed");
       return -1;
     }
     send_cnt++;
@@ -176,7 +154,7 @@ int main(int argc, char *argv[]) {
       puts("Select error");
       return -1;
     } else if (n == 0) {
-      if (send_cnt >= gMaxSendTimes) {
+      if (send_cnt >= SEND_TIMES) {
         puts("Timeout error");
         return -1;
       }
@@ -184,8 +162,8 @@ int main(int argc, char *argv[]) {
     } else {
       // Receive calculation protocol message
       memset(&calcProtocolMsg, 0, sizeof(calcProtocolMsg));
-      socklen_t len = sizeof(*serv_addr);
-      if (recvfrom(sock, &calcProtocolMsg, sizeof(calcProtocolMsg), 0, (sockaddr *)serv_addr, &len) < 0) {
+      socklen_t len = serv_addr->ai_addrlen;
+      if (recvfrom(sock, &calcProtocolMsg, sizeof(calcProtocolMsg), 0, serv_addr->ai_addr, &len) < 0) {
         // Version check
         printf("NOT OK\n");
         return -1;
@@ -201,7 +179,7 @@ int main(int argc, char *argv[]) {
   send_cnt = 0;
   while (true) {
     // Send result
-    if (sendto(sock, &calcProtocolMsg, sizeof(calcProtocolMsg), 0, (sockaddr *)serv_addr, sizeof(*serv_addr)) < 0) {
+    if (sendto(sock, &calcProtocolMsg, sizeof(calcProtocolMsg), 0, serv_addr->ai_addr, serv_addr->ai_addrlen) < 0) {
       printf("Send failed\n");
       return -1;
     }
@@ -213,7 +191,7 @@ int main(int argc, char *argv[]) {
       puts("Select error");
       return -1;
     } else if (n == 0) {
-      if (send_cnt >= gMaxSendTimes) {
+      if (send_cnt >= SEND_TIMES) {
         puts("Timeout error");
         return -1;
       }
@@ -222,8 +200,8 @@ int main(int argc, char *argv[]) {
       // Receive result message
       calcMessage resMsg;
       memset(&resMsg, 0, sizeof(resMsg));
-      socklen_t len = sizeof(*serv_addr);
-      if (recvfrom(sock, &resMsg, sizeof(resMsg), 0, (sockaddr *)serv_addr, &len) < 0 || ntohl(resMsg.message) != 1) {
+      socklen_t len = serv_addr->ai_addrlen;
+      if (recvfrom(sock, &resMsg, sizeof(resMsg), 0, serv_addr->ai_addr, &len) < 0 || ntohl(resMsg.message) != 1) {
         printf("NOT OK\n");
         return -1;
       } else if (ntohl(resMsg.message) == 1) {
@@ -234,5 +212,7 @@ int main(int argc, char *argv[]) {
     }
   }
 
+  // Release the resources
   close(sock);
+  freeaddrinfo(serv_addr);
 }
